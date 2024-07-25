2d5b190f2be479a75d3cbd06f23a32ff4b3ad2a1
Pooria <144683087+Pooria82@users.noreply.github.com>
2024-07-26 03:17:38 +0330

    Implementation of the project model in the backend

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'projects_app.apps.ProjectsAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -112,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'assets')
 ]
